496571e9f8f3faf2656d0439a11bbe2bbf01d408
TDB-star <tatiana.blache@gmail.com>
2024-06-17 16:19:50 +0200

    create: created movie_item xml, mainActivity xml, added query parameter to ApiService

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -2,8 +2,9 @@ package com.example.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("movie?token=281KYP4-NV4MRQR-KDPV1E7-CHPW68E&rating.imdb=7-10&sortField=votes.kp&sortType=-1")
-    Single<MovieResponse> loadMovies();
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 }
--- app/src/main/java/com/example/movies/MainActivity.java
+++ app/src/main/java/com/example/movies/MainActivity.java
@@ -1,34 +1,34 @@
 package com.example.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+private final String TAG = "DEBUG_MainActivity:";
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                Log.d(TAG,movies.toString());
+            }
+        });
 
-     ApiFactory.apiService.loadMovies()
-             .subscribeOn(Schedulers.io())
-             .observeOn(AndroidSchedulers.mainThread())
-             .subscribe(new Consumer<MovieResponse>() {
-                 @Override
-                 public void accept(MovieResponse movieResponse) throws Throwable {
-                     Log.d("MainAcrivity", movieResponse.toString());
-                 }
-             }, new Consumer<Throwable>() {
-                 @Override
-                 public void accept(Throwable throwable) throws Throwable {
-                     Log.d("MainAcrivity", throwable.toString());
-                 }
-             });
+        viewModel.loadMovies();
     }
 }
