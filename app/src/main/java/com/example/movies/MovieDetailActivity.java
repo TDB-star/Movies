cc697dac6b0bf87f529f481dcde7054cb7f454c8
TDB-star <tatiana.blache@gmail.com>
2024-06-18 10:14:24 +0200

    feature: realised layout for movie detail activity

--- app/src/main/java/com/example/movies/MovieDetailActivity.java
+++ app/src/main/java/com/example/movies/MovieDetailActivity.java
@@ -2,13 +2,35 @@ package com.example.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MovieDetailActivity extends AppCompatActivity {
+    private static final String EXTRA_MOVIE = "movie";
+    private TextView textViewTitle;
+    private TextView textViewYear;
+    private TextView textViewDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        initViews();
+    }
+
+    private void initViews() {
+       textViewTitle = findViewById(R.id.textViewTitle);
+       textViewYear = findViewById(R.id.textViewYear);
+       textViewDescription = findViewById(R.id.textViewDescription);
+    }
+
+    public static Intent newIntent(Context context, Movie movie) {
+
+        Intent intent = new Intent(context, MovieDetailActivity.class);
+        intent.putExtra(EXTRA_MOVIE, movie);
+        return intent;
+
     }
 }
